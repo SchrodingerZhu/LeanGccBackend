813cee7f34f62e2260764861ad038ef944788dde
Schrodinger ZHU Yifan <yifanzhu@rochester.edu>
2023-10-12 23:17:55 -0400

    misc: update lean-gccjit to latest revision

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-require «lean-gccjit» from git "https://github.com/schrodingerzhu/lean-gccjit" @ "a35ccb2ad8ede0425967e6f905b867d80b6ca729"
+require «lean-gccjit» from git "https://github.com/schrodingerzhu/lean-gccjit" @ "0.1.2"
 
 package «LeanGccBackend» where
   -- add package configuration options here
